8efa5f41345601f337aabefef9b9bc94bebc6527
David Fry <david.fry.tv@gmail.com>
2014-08-05 16:22:20 -0700

    Adding dictionary
    Created a swift dictionary object

--- C19_a2_Class_Roster_P1/ViewController.swift
+++ C19_a2_Class_Roster_P1/ViewController.swift
@@ -7,12 +7,147 @@
 //
 
 import UIKit
+import Foundation
 
 class ViewController: UIViewController {
-                            
-    override func viewDidLoad() {
+    
+    var personArray = [Person]()
+    var dave = Person(firstName: "David", lastName: "Fry")
+    
+    var newArray = [
+            [
+                "lastName": "Birkholz",
+                "firstName": "Nate"
+            ],
+        
+            [
+                "lastName": "Brightbill",
+                "firstName": "Matthew"
+            ],
+        
+            [
+                "lastName": "Chavez",
+                "firstName": "Jeff"
+            ],
+        
+            [
+                "lastName": "Clem",
+                "firstName": "John"
+            ],
+            [
+                "lastName": "Doi",
+                "firstName": "Yuto"
+            ],
+            [
+            "lastName": "Ferderer",
+            "firstName": "Chrstie"
+            ],
+            [
+            "lastName": "Fry",
+            "firstName": "David"
+            ],
+            [
+            "lastName": "Petrica Gherle",
+            "firstName": "Adrian"
+            ],
+            [
+            "lastName": "Hawken",
+            "firstName": "Jake"
+            ],
+            [
+            "lastName": "Huang",
+            "firstName": "Tony"
+            ],
+            [
+            "lastName": "Johnson",
+            "firstName": "Brad"
+            ],
+            [
+            "lastName": "Kazi",
+            "firstName": "Shams"
+            ],
+            [
+            "lastName": "Klein",
+            "firstName": "Cameron"
+            ],
+            [
+            "lastName": "Kolodziejczak",
+            "firstName": "Kori"
+            ],
+            [
+            "lastName": "Lewis",
+            "firstName": "Parker"
+            ],
+            [
+            "lastName": "none",
+            "firstName": "lindy@codefellows.com"
+            ],
+            [
+            "lastName": "Ma",
+            "firstName": "Nathan"
+            ],
+            [
+            "lastName": "MacPhee",
+            "firstName": "Casey"
+            ],
+            [
+            "lastName": "McAleer",
+            "firstName": "Brendan"
+            ],
+            [
+            "lastName": "Mendez",
+            "firstName": "Brian"
+            ],
+            [
+            "lastName": "Morris",
+            "firstName": "Mark"
+            ],
+            [
+            "lastName": "North",
+            "firstName": "Rowan"
+            ],
+            [
+            "lastName": "Park",
+            "firstName": "Lance"
+            ],
+            [
+            "lastName": "Pham",
+            "firstName": "Kevin"
+            ],
+            [
+            "lastName": "Richman",
+            "firstName": "Will"
+            ],
+            [
+            "lastName": "Thueringer",
+            "firstName": "Heather"
+            ],
+            [
+            "lastName": "Vu",
+            "firstName": "Tuan"
+            ],
+            [
+            "lastName": "Walkingstick",
+            "firstName": "Zack"
+            ],
+            [
+            "lastName": "Wong",
+            "firstName": "Sara"
+            ],
+            [
+            "lastName": "Zhang",
+            "firstName": "Hongyao"
+            ]
+        ]
+
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        makePersonsArray()
+        for person in self.personArray
+        {
+            println("\(person.fullName())")
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,12 +155,26 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-    
-    func makePersonsArray() -> Array<Person>
+    func makeJSONData()
     {
-        let personList = Array<Person>()
         
-        return personList
     }
+    
+    func makePersonsArray()
+    {
+        
+        for name in newArray
+        {
+            var newPerson = Person(firstName: name["firstName"], lastName: name["lastName"])
+            personArray.append(newPerson)
+        }
+    
+    }
+    
+    
+    
+    
+    
+    
 }
 
---
+++ TestFileReadAndWrite.playground/section-1.swift
@@ -0,0 +1,8 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+var data:NSData = NSData.dataWithContentsOfFile("roster.json", options: NSDataReadingOptions.self, error: NSErrorPointer.self)
+
+var error: NSError?
+
