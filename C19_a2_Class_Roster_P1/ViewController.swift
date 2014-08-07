c9c80de9f8e637d3d205be6d0e5185fbc6b89426
David Fry <david.fry.tv@gmail.com>
2014-08-07 16:33:02 -0700

    Got the plist working

--- C19_a2_Class_Roster_P1/ViewController.swift
+++ C19_a2_Class_Roster_P1/ViewController.swift
@@ -6,6 +6,11 @@
 //  Copyright (c) 2014 David Fry. All rights reserved.
 //
 
+// Canvas API to get all students in C19 https://canvas.instructure.com/api/v1/courses/868751/students
+// Canvas API to get student object https://canvas.instructure.com/api/v1/users/3987826/profile (/users/:user_id/profile)
+// API Key: 7~CKAvoX6pZT2oeBQOWFtKzI6f9et2XdunrLMYvO5IIWNyoVvBwLoZLkpV45MFNZaK
+
+
 import UIKit
 import Foundation
 
@@ -13,19 +18,24 @@ class ViewController: UIViewController {
     
     var newArray = []
     var personArray = [Person]()
-    let studentPlist = NSDictionary(contentsOfFile: NSBundle.mainBundle().pathForResource("studentRoster", ofType: "plist"))
-    //let mypath = NSBundle.mainBundle().pathForResource("studentRoster", ofType: "plist")
+    let studentPlist = NSArray(contentsOfFile: NSBundle.mainBundle().pathForResource("studentRoster", ofType: "plsit"))
+    let plistPath = NSBundle.mainBundle().pathForResource("studentRoster", ofType: "plist")
+    
+    
     //let dict = NSDictionary(contentsOfFile:self.mypath)
-    var dict = self.studentPlist as Array<Dictionary<String, String>>
+    
     override func viewDidLoad()
     {
         super.viewDidLoad()
+        
+        let studentRoster = NSArray(contentsOfFile: self.plistPath)
+        //var studentList = self.studentPlist as Array<Dictionary<String, String>>
 //      makePersonsArray()
 //        for person in self.personArray
 //        {
 //            println("\(person.fullName())")
 //        }
-        println(self.dict[0])
+        println(studentRoster[0]["firstName"])
     }
 
     override func didReceiveMemoryWarning() {
@@ -45,7 +55,31 @@ class ViewController: UIViewController {
 //    
 //    }
     
-    
+// Canvas API Stuff:
+// I dont understand how to get the auth to work and let me pull the data I need
+//    let studentListURLPath = "https://canvas.instructure.com/api/v1/courses/868751/students"
+//    let apiKey = "7~CKAvoX6pZT2oeBQOWFtKzI6f9et2XdunrLMYvO5IIWNyoVvBwLoZLkpV45MFNZaK"
+//    var url = NSURL(string:studentListURLPath)
+//    var request = NSURLRequest(URL: url)
+//    let queue:NSOperationQueue = NSOperationQueue()
+//    
+//    
+//    let authURL = NSURL(string: "https://canvas.instructure.com/api/v1/courses?access_token=" + self.apiKey)
+//    let task = NSURLSession.sharedSession().dataTaskWithURL(authURL){(data, response, error)
+//        in
+//    }
+//    task.resume()
+//    
+//    NSURLConnection.sendAsynchronousRequest(request, queue: queue, completionHandler:{ (response: NSURLResponse!, data: NSData!, error: NSError!) -> Void in
+//   
+//    
+//    
+//    var err: NSError
+//    var jsonResult: NSDictionary = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers, error: nil) as NSDictionary
+//    println("AsyncsynchonousRequest\(jsonResult)")
+//    
+//    
+//    })
     
     
     
