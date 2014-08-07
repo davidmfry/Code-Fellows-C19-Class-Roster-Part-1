08fa484ae0efe3e3815ec85ed4038d242f119761
David Fry <david.fry.tv@gmail.com>
2014-08-07 11:54:00 -0700

    Trying to get the plist to work

--- C19_a2_Class_Roster_P1/ViewController.swift
+++ C19_a2_Class_Roster_P1/ViewController.swift
@@ -11,143 +11,21 @@ import Foundation
 
 class ViewController: UIViewController {
     
+    var newArray = []
     var personArray = [Person]()
-    var dave = Person(firstName: "David", lastName: "Fry")
-    
-    var newArray = [
-            [
-                "lastName": "Birkholz",
-                "firstName": "Nate"
-            ],
-        
-            [
-                "lastName": "Brightbill",
-                "firstName": "Matthew"
-            ],
-        
-            [
-                "lastName": "Chavez",
-                "firstName": "Jeff"
-            ],
-        
-            [
-                "lastName": "Clem",
-                "firstName": "John"
-            ],
-            [
-                "lastName": "Doi",
-                "firstName": "Yuto"
-            ],
-            [
-            "lastName": "Ferderer",
-            "firstName": "Chrstie"
-            ],
-            [
-            "lastName": "Fry",
-            "firstName": "David"
-            ],
-            [
-            "lastName": "Petrica Gherle",
-            "firstName": "Adrian"
-            ],
-            [
-            "lastName": "Hawken",
-            "firstName": "Jake"
-            ],
-            [
-            "lastName": "Huang",
-            "firstName": "Tony"
-            ],
-            [
-            "lastName": "Johnson",
-            "firstName": "Brad"
-            ],
-            [
-            "lastName": "Kazi",
-            "firstName": "Shams"
-            ],
-            [
-            "lastName": "Klein",
-            "firstName": "Cameron"
-            ],
-            [
-            "lastName": "Kolodziejczak",
-            "firstName": "Kori"
-            ],
-            [
-            "lastName": "Lewis",
-            "firstName": "Parker"
-            ],
-            [
-            "lastName": "none",
-            "firstName": "lindy@codefellows.com"
-            ],
-            [
-            "lastName": "Ma",
-            "firstName": "Nathan"
-            ],
-            [
-            "lastName": "MacPhee",
-            "firstName": "Casey"
-            ],
-            [
-            "lastName": "McAleer",
-            "firstName": "Brendan"
-            ],
-            [
-            "lastName": "Mendez",
-            "firstName": "Brian"
-            ],
-            [
-            "lastName": "Morris",
-            "firstName": "Mark"
-            ],
-            [
-            "lastName": "North",
-            "firstName": "Rowan"
-            ],
-            [
-            "lastName": "Park",
-            "firstName": "Lance"
-            ],
-            [
-            "lastName": "Pham",
-            "firstName": "Kevin"
-            ],
-            [
-            "lastName": "Richman",
-            "firstName": "Will"
-            ],
-            [
-            "lastName": "Thueringer",
-            "firstName": "Heather"
-            ],
-            [
-            "lastName": "Vu",
-            "firstName": "Tuan"
-            ],
-            [
-            "lastName": "Walkingstick",
-            "firstName": "Zack"
-            ],
-            [
-            "lastName": "Wong",
-            "firstName": "Sara"
-            ],
-            [
-            "lastName": "Zhang",
-            "firstName": "Hongyao"
-            ]
-        ]
-
+    let studentPlist = NSDictionary(contentsOfFile: NSBundle.mainBundle().pathForResource("studentRoster", ofType: "plist"))
+    //let mypath = NSBundle.mainBundle().pathForResource("studentRoster", ofType: "plist")
+    //let dict = NSDictionary(contentsOfFile:self.mypath)
+    var dict = self.studentPlist as Array<Dictionary<String, String>>
     override func viewDidLoad()
     {
         super.viewDidLoad()
-        makePersonsArray()
-        for person in self.personArray
-        {
-            println("\(person.fullName())")
-        }
+//      makePersonsArray()
+//        for person in self.personArray
+//        {
+//            println("\(person.fullName())")
+//        }
+        println(self.dict[0])
     }
 
     override func didReceiveMemoryWarning() {
@@ -155,21 +33,17 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-    func makeJSONData()
-    {
-        
-    }
     
-    func makePersonsArray()
-    {
-        
-        for name in newArray
-        {
-            var newPerson = Person(firstName: name["firstName"], lastName: name["lastName"])
-            personArray.append(newPerson)
-        }
-    
-    }
+//    func makePersonsArray()
+//    {
+//        
+//        for name in newArray
+//        {
+//            var newPerson = Person(firstName: name["firstName"]!, lastName: name["lastName"]!)
+//            personArray.append(newPerson)
+//        }
+//    
+//    }
     
     
     
